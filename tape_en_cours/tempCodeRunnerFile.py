d861ab10045ba34b079b6e8ded43522977a342b3
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 16:26:43 +0200

    gitautosync update 198

--- tape_en_cours/tempCodeRunnerFile.py
+++ tape_en_cours/tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-print(regle_fizz_buzz(1) == "1")
-# print(regle_fizz_buzz(3) == "Fizz")
-# print(regle_fizz_buzz(5) == "Buzz")
-# print(regle_fizz_buzz(15) == "FizzBuzz")
+from fizzbuzz import regle_fizz_buzz
