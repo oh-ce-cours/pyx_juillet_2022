6745922b8458debd11cd214c2ad69c6cc99cfe39
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 16:55:33 +0200

    gitautosync update 240

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -2,7 +2,7 @@ from exo1 import regle_fizz_buzz
 
 # print("dans test_fizzbuzz.py", __name__)
 
-assert regle_fizz_buzz(1) == "1"
 assert regle_fizz_buzz(3) == "Fizz"
 assert regle_fizz_buzz(5) == "Buzz"
+assert regle_fizz_buzz(1) == 1
 assert regle_fizz_buzz(15) == "FizzBuzz"
