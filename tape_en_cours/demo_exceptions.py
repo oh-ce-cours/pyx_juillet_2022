7c9d147b490ff893de4518ceaeaed7d82309d8b6
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 15:26:41 +0200

    gitautosync update 144

--- tape_en_cours/demo_exceptions.py
+++ tape_en_cours/demo_exceptions.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def f():
     1 / 0
 
@@ -9,4 +12,7 @@ def g():
 try:
     g()
 except Exception as e:
+    exc_type, exc_obj, exc_tb = sys.exc_info()
+    fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+    print(exc_type, fname, exc_tb.tb_lineno)
     print(e)
