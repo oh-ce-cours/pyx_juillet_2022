2b6f26df5c294883a83cc250d7623858e973201e
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 12:28:58 +0200

    gitautosync update 103

--- tape_en_cours/plus_ou_moins_correction.py
+++ tape_en_cours/plus_ou_moins_correction.py
@@ -3,11 +3,7 @@ import random
 random_number = random.randrange(1, 101)
 input_number = -1
 while input_number != random_number:
-    try:
-        input_number = int(input("Enter an integer number:"))
-    except:
-        print("Enter a number please")
-        continue
+    input_number = int(input("Enter an integer number:"))
 
     if input_number > random_number:
         print("Input Number is greater than random number")
