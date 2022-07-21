2d0c8c959d04c04f98daa109752109fdba77a980
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 15:32:51 +0200

    gitautosync update 149

--- tape_en_cours/demo_exceptions.py
+++ tape_en_cours/demo_exceptions.py
@@ -1,21 +1,19 @@
 import sys
 import os
 
+# from https://docs.python.org/3/library/email.examples.html
 # Import smtplib for the actual sending function
 import smtplib
 
 # Import the email modules we'll need
 from email.message import EmailMessage
 
-# Open the plain text file whose name is in textfile for reading.
-with open(textfile) as fp:
-    # Create a text/plain message
-    msg = EmailMessage()
-    msg.set_content(fp.read())
+msg = EmailMessage()
+msg.set_content(message)
 
 # me == the sender's email address
 # you == the recipient's email address
-msg["Subject"] = f"The contents of {textfile}"
+msg["Subject"] = f"Everything is broken, plez help"
 msg["From"] = me
 msg["To"] = you
 
