4a4b3b5376af253b3245557364123b150f225637
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 12:40:41 +0200

    gitautosync update 110

--- tape_en_cours/plus_ou_moins_correction.py
+++ tape_en_cours/plus_ou_moins_correction.py
@@ -1,4 +1,5 @@
 import random
+import sys
 
 random_number = random.randrange(1, 101)
 input_number = -1
@@ -7,6 +8,7 @@ while input_number != random_number:
         input_number = int(input("Enter an integer number:"))
     except ValueError:
         print("Enter a number please")
+        sys.exit(4)
         continue
 
     if input_number > random_number:
