7a4c97cada6978127d58af4884c983a0d9e204e3
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 15:29:06 +0200

    gitautosync update 147

--- tape_en_cours/demo_exceptions.py
+++ tape_en_cours/demo_exceptions.py
@@ -16,5 +16,5 @@ except Exception as e:
     exc_type, exc_obj, exc_tb = sys.exc_info()
     fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
     print(
-        f"L'exception {exc_type} s'est produite dans le fichier {fname} à la ligne {exc_tb.tb_lineno} avec pour message {str(e)}"
+        f"L'exception {exc_type.__name__} s'est produite dans le fichier {fname} à la ligne {exc_tb.tb_lineno} avec pour message {str(e)}"
     )
