b423df16005d4458159e7a108cfd7a463913c06a
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 16:42:47 +0200

    gitautosync update 218

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -1,8 +1,10 @@
-from fizzbuzz import regle_fizz_buzz
+from fizzbuzz import fizzbuzz
 
 # print("dans test_fizzbuzz.py", __name__)
 
-print(regle_fizz_buzz(1) == "1")
-print(regle_fizz_buzz(3) == "Fizz")
-print(regle_fizz_buzz(5) == "Buzz")
-print(regle_fizz_buzz(15) == "FizzBuzz")
+# print(regle_fizz_buzz(1) == "1")
+# print(regle_fizz_buzz(3) == "Fizz")
+# print(regle_fizz_buzz(5) == "Buzz")
+# print(regle_fizz_buzz(15) == "FizzBuzz")
+
+fizzbuzz(4)
