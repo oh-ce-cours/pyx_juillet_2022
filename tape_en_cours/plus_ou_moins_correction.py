c299624e5af039fc1c3c027940bc85e2b7a36b17
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 12:23:19 +0200

    gitautosync update 97

--- tape_en_cours/plus_ou_moins_correction.py
+++ tape_en_cours/plus_ou_moins_correction.py
@@ -2,7 +2,7 @@ import random
 "
 
 random_number = random.randrange(1, 101)
-input_number = 0
+input_number = -1
 while input_number != random_number:
     input_number = int(input("Enter an integer number:"))
     if input_number > random_number:
