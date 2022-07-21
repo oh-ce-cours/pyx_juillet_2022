788b0f15e25cba5a8f0d1814f748a06f96a8d9d9
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 17:08:01 +0200

    gitautosync update 257

--- tape_en_cours/test_fizzbuzz.py
+++ tape_en_cours/test_fizzbuzz.py
@@ -6,7 +6,7 @@ from exo1 import regle_fizz_buzz, est_divisible_par
 def test_regles():
     assert regle_fizz_buzz(3) == "Fizz"
     assert regle_fizz_buzz(5) == "Buzz"
-    assert regle_fizz_buzz(1) == "1"
+    assert regle_fizz_buzz(1) == 1
     assert regle_fizz_buzz(15) == "FizzBuzz"
 
 
