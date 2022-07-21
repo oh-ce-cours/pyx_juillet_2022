73d129fc90117e069438b4ad77ce9c5d6581d87b
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 15:35:14 +0200

    gitautosync update 157

---
+++ tape_en_cours/mail_utils.py
@@ -0,0 +1,6 @@
+# from https://docs.python.org/3/library/email.examples.html
+# Import smtplib for the actual sending function
+import smtplib
+
+# Import the email modules we'll need
+from email.message import EmailMessage
