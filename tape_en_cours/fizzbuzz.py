28522713c8b4379a21a9c3cbe5466bcd3fc2ce9e
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-21 16:13:14 +0200

    gitautosync update 174

--- tape_en_cours/fizzbuzz.py
+++ tape_en_cours/fizzbuzz.py
@@ -1,13 +1,18 @@
+def regle_fizz_buzz(nombre: int) -> str:
+    res = ""
+    if nombre % 3 == 0:
+        res += "Fizz"
+    if nombre % 5 == 0:
+        res += "Buzz"
+
+    if not res:
+        res = str(nombre)
+
+    return res
+
+
 def fizzbuzz(nombre_max: int):
     for nombre in range(1, 1 + nombre_max):
-        res = ""
-        if nombre % 3 == 0:
-            res += "Fizz"
-        if nombre % 5 == 0:
-            res += "Buzz"
-
-        if not res:
-            res = str(nombre)
 
         print(res)
 
