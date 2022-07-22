d4d9f95b2af2bc1583d3f50126d9c17e0d08193e
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-22 16:21:09 +0200

    gitautosync update 349

--- tape_en_cours/exo4-2.py
+++ tape_en_cours/exo4-2.py
@@ -2,7 +2,6 @@ from pathlib import Path
 import tarfile
 import puremagic
 
-# on dézippe le fichier
 zip_path = Path(
     "../medias/administrationSysteme/manipulationFichiers/fichiersVrac.tar.gz"
 )
@@ -10,7 +9,11 @@ extraction_path = Path(
     "../medias/administrationSysteme/manipulationFichiers/fichiersVrac_dezippe_python"
 )
 
+# on dézippe le fichier
 extraction_path.mkdir(parents=True, exist_ok=True)
 file = tarfile.open(zip_path)
 file.extractall(extraction_path)
 file.close()
+
+# on va récupérer les extensions réelles des fichiers
+for filename in extraction_path.iterdir()
