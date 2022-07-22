93279108e70f322ffc2eb30ba95837ae8643d38a
Matthieu Falce <falce.matthieu@gmail.com>
2022-07-22 16:16:40 +0200

    gitautosync update 344

--- tape_en_cours/exo4-2.py
+++ tape_en_cours/exo4-2.py
@@ -1,3 +1,6 @@
-from pathlib import Path 
+from pathlib import Path
 import tarfile
 import puremagic
+
+# on dézippe le fichier
+zip_path = "../medias/administrationSysteme/manipulationFichiers/fichiersVrac.tar.gz"
